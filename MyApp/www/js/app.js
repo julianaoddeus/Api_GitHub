44cdc8aaa22fd9a7363a0a002cca81217e3c95c2
Juliana <64862169+julianaoddeus@users.noreply.github.com>
2022-05-25 20:50:29 -0300

    criado js request

--- MyApp/www/js/app.js
+++ MyApp/www/js/app.js
@@ -1,27 +1,28 @@
 function BuscaInfoGit(){
-   $.ajax({
-       type: "GET",
-       url:`https://api.github.com/users/gutoffline`,
-       success: (function(user){
-           console.log(user)
-           let avatar = user.avatar_url
-           let nome = user.nome
-           let login = user.login
-           let twitter = user.twitter
-           let blog = user.blog    
-           let github = user.github
-           let instituicao = user.instituicao
-           let local = user.local
-           document.getElementById('resposta').innerHTML = 
-           `
-           <div class="col-md-5">
-                <img src="{avatar}">
-           </div>
-           `
-           
-
-       })
-   })
+    $.ajax({
+        type: "GET",
+        url: "https://api.github.com/users/suellen5",
+         success: (function(user){
+             console.log(user)
+             let avatar = user.avatar_url
+             let nome = user.name
+             let login = user.login             
+             let twitter = user.twitter_username
+             let blog = user.blog
+             let github = user.url        
+             let instuicao = user.company
+             let local = user.local
+             document.getElementById("resposta").innerHTML = 
+             `<div class="col-md-5">
+             <img src="${avatar}" width="150"></div> 
+             <div class="col-md-7"> 
+             ${nome} (${login}) <br> 
+             <a href="${twitter}"><i class="bi bi-twitter"></i></a> 
+             <a href="${blog}"><i class="bi bi-github" width="100"></i></a> 
+             <a href="${medium}"><i class="bi bi-medium"></i></a> <br> 
+             ${instuicao} - ${pais}`
+        })
+    })
    
        
     
