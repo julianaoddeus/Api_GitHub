e2fd6d32dc966ad2fe48589cccc0f67c6376326f
Juliana <julianaacademi@gmail.com>
2022-05-26 20:21:49 -0300

    update

--- MyApp/www/js/app.js
+++ MyApp/www/js/app.js
@@ -1,35 +1,38 @@
-let lista = ["gutoffline", "SantiagoVinicius", "julianaoddeus", "marceloVinicius12", "MoreiraJulia", "suellen05", "Marciocarneiro77", "Taynaradb", "Gerisbr"]
-let element =
+let lista = ["gutoffline", "SantiagoVinicius", "julianaoddeus", "marceloVinicius12", "MoreiraJulia", "suellen05", "Marciocarneiro77", "Taynaradb", "Gerisbr"];
 
-    function BuscaInfoGit() {
-        $.ajax({
-            type: "GET",
-            url: `https://api.github.com/users/${element}`,
-            success: (function (user) {
-                console.log(user)
-                let avatar = user.avatar_url
-                let nome = user.name
-                let login = user.login
-                let twitter = user.twitter_username
-                let blog = user.blog
-                let github = user.url
-                let instuicao = user.company
-                let local = user.local
-                document.getElementById("resposta").innerHTML =
-                    `<div class="col-md-5">
-                <img src="${avatar}" width="150"></div> 
-             <div class="col-md-7"> 
-             ${nome} (${login}) <br> 
-             <a href="${twitter}"><i class="bi bi-twitter"></i></a> 
-             <a href="${blog}"><i class="bi bi-github" width="100"></i></a> 
-             <a href="${github}"><i class="bi bi-medium"></i></a> <br> 
-             ${instuicao} - ${local}`
-            })
+let element = lista[Math.floor(Math.random() * lista.length)];
+function BuscaInfoGit() {
+    $.ajax({
+        type: "GET",
+        url: `https://api.github.com/users/${element}`,
+        success: (function (user) {          
+            let avatar = user.avatar_url
+            let nome = user.name
+            let login = user.login
+            let twitter = user.twitter
+            let blog = user.blog
+            let github = user.url
+            let instuicao = user.
+            let local = user.local
+            document.getElementById("resposta").innerHTML =
+                `<div class="row">
+                    <div class="col-md-6">
+                        <img src="${avatar}" width="150"></div> 
+                    <div class="col-md-6 flex-column"> 
+                        ${nome} (${login}) <br> 
+                        <a href="${twitter}">twitter</a> </br>
+                        <a href="${blog}">medium-blog</a> </br>
+                        <a href="${github}">github</a></br> 
+                        ${instuicao} - ${local}
+                    </div>                    
+                </div>`
+             
         })
+    })
 
 
 
-    }
+}
 
 
 
