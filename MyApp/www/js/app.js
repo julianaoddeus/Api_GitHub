0eafecdc80ceb20f9a452558978a234ea888b6e9
Juliana <julianaacademi@gmail.com>
2022-05-26 19:33:30 -0300

    array lista

--- MyApp/www/js/app.js
+++ MyApp/www/js/app.js
@@ -1,19 +1,22 @@
-function BuscaInfoGit(){
-    $.ajax({
-        type: "GET",
-        url: "https://api.github.com/users/suellen5",
-         success: (function(user){
-             console.log(user)
-             let avatar = user.avatar_url
-             let nome = user.name
-             let login = user.login             
-             let twitter = user.twitter_username
-             let blog = user.blog
-             let github = user.url        
-             let instuicao = user.company
-             let local = user.local
-             document.getElementById("resposta").innerHTML = 
-             `<div class="col-md-5">
+let lista = ["gutoffline", "SantiagoVinicius", "julianaoddeus", "marceloVinicius12", "MoreiraJulia", "suellen05", "Marciocarneiro77", "Taynaradb", "Gerisbr"]
+let element =
+
+    function BuscaInfoGit() {
+        $.ajax({
+            type: "GET",
+            url: `https://api.github.com/users/${element}`,
+            success: (function (user) {
+                console.log(user)
+                let avatar = user.avatar_url
+                let nome = user.name
+                let login = user.login
+                let twitter = user.twitter_username
+                let blog = user.blog
+                let github = user.url
+                let instuicao = user.company
+                let local = user.local
+                document.getElementById("resposta").innerHTML =
+                    `<div class="col-md-5">
                 <img src="${avatar}" width="150"></div> 
              <div class="col-md-7"> 
              ${nome} (${login}) <br> 
@@ -21,15 +24,15 @@ function BuscaInfoGit(){
              <a href="${blog}"><i class="bi bi-github" width="100"></i></a> 
              <a href="${github}"><i class="bi bi-medium"></i></a> <br> 
              ${instuicao} - ${local}`
+            })
         })
-    })
-   
-       
-    
-}
 
 
 
-document.getElementById('btn-buscar').addEventListener('click', function(){
+    }
+
+
+
+document.getElementById('btn-buscar').addEventListener('click', function () {
     BuscaInfoGit();
 })
